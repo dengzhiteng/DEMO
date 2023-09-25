4b3a124d8af65ebf4983e158c7bb7786e99dee35
dengzhiteng <mrdengzhiteng163.com>
2023-09-25 08:55:11 +0800

    test

--- 数据结构/击鼓传花.js
+++ 数据结构/击鼓传花.js
@@ -5,11 +5,9 @@
 class Queue {
   constructor() {
     this.items = []
-    this.lowCount = 0
-    this.count = 0
   }
   delqueue () {
-    return this.items.pop()
+    return this.items.shift()
   }
   enqueue (data) {
     this.items.push(data)
@@ -21,26 +19,35 @@ class Queue {
       return this.size()===0
   }
   size () {
-    return this.count - this.lowCount
+    return this.items.length
   }
   clear () {
     this.items = []
-    this.lowCount = 0
-    this.count = 0
     return true
   }
-  toString () {
-  }
 }
 
-let queue = new Queue()
-const Game = (list, num) => {
+const Game = (names, num) => {
+  if (!Array.isArray(names) && names.length) {
+    throw new Error("请输入参赛人员")
+  }
+  if (!num) {
+    throw new Error("请输入击鼓次数")
+  }
   const queue = new Queue()
-  for (let item of list) {
+  for (let item of names) {
     queue.enqueue(item)
   }
+
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+        queue.enqueue(queue.delqueue())
+    }
+    queue.delqueue()
+  }
+  return queue.delqueue()
 }
 
-const arr = ["唐僧", "悟净", "猪八戒", "孙悟空", "张居正", "杨莲", "贾宝玉", "武松", "潘金莲", "李大钊"]
-Game(arr)
-
+const names = ["唐僧", "悟净", "猪八戒", "孙悟空", "张居正", "杨莲", "贾宝玉", "武松", "潘金莲", "李大钊"]
+const winner= Game(names,100)
+console.warn(`胜出:${winner}`);
